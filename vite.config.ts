964666b59ca3f8267a7b8f43daa482a7bf1a8279
lhuissier4 <lhurem20@gmail.com>
2025-10-09 15:41:26 +0200

    add link to versel

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     server: {
         proxy: {
             "/api": {
-                target: "http://127.0.0.1:3000/",
+                target: "https://escapegameepsiapi-lhuissiers-projects.vercel.app",
                 changeOrigin: true,
                 secure: false,
 
